319dbe8ed59932f741bdcf4647d453e01d56e13e
JKL453 <j.kappel@posteo.de>
2024-07-20 19:02:14 +0200

    Update .gitignore to exclude build artifacts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='s04utils',
-    version='0.1.0',
+    version='0.1.1',
     author='Janosch Kappel',
     author_email='jkl453@posteo.de',
     description='A module to load and analyze timestamp data from HDF5-photon files',
